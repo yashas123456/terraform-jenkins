fb9603d15aa95291d31821b60b389369d2d4b3c1
yashas123456 <91239570+yashas123456@users.noreply.github.com>
2022-04-06 21:47:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 module "vm" {
     source = "./modules/vm"
     #vm_count = 2
-    count = length("${vm_name}")
-    vm_name = "${"${vm_name}"[count.index]}"
+    count = length(var.vm_name)
+    vm_name = "${var.vm_name[count.index]}"
     resource_group_name = var.resource_group_name
     network_subnet_name = var.network_subnet_name
     virtual_network_name = var.virtual_network_name
-    network_interface_name = "${"${vm_name}"[count.index]}"
+    network_interface_name = "${var.vm_name[count.index]}"
     network_security_group = var.network_security_group
     #vm_name             = var.vm_name
-    vm_size             = "${"${vm_size}"[count.index]}"
-    hostname            = "${"${vm_name}"[count.index]}"
+    vm_size             = "${var.vm_size[count.index]}"
+    hostname            = "${var.vm_name[count.index]}"
 }
-
